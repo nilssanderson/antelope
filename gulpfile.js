c698290273ec1c0e11038e3f208630dbfcc64e56
Nils Sanderson <thebaldwan@gmail.com>
2015-12-03 03:21:35 +0000

    Created: Antelope - Starter webapp framework

---
+++ gulpfile.js
@@ -0,0 +1,261 @@
+var $                 = require('gulp-load-plugins')();
+var argv              = require('yargs').argv;
+var browser           = require('browser-sync');
+var gulp              = require('gulp');
+var panini            = require('panini');
+var rimraf            = require('rimraf');
+var sequence          = require('run-sequence');
+var sherpa            = require('style-sherpa');
+var styleguide        = require('sc5-styleguide');
+var sass              = require('gulp-sass');
+var requireDir        = require('require-dir');
+
+
+// Use `spawn` to execute shell command using Node
+var spawn = require('child_process').spawn;
+
+// The directory that contains the Gulpfile whose task needs to be run.
+var backstopJS        = './bower_components/BackstopJS/gulp/tasks';
+
+// Gulp tasks that need to be run.
+var reference = ['reference']
+var test = ['test']
+
+var outputPath        = 'dist/docs';
+
+// Check for --production flag
+var isProduction = !!(argv.production);
+
+// Port to use for the development server.
+var PORT = 8000;
+
+// Browsers to target when prefixing CSS.
+var COMPATIBILITY = ['last 2 versions', 'ie >= 9'];
+
+// File paths to various assets are defined here.
+var PATHS = {
+  assets: [
+    'src/assets/**/*',
+    '!src/assets/{!img,js,scss}/**/*'
+  ],
+  sass: [
+    'bower_components/foundation-sites/scss',
+    'bower_components/motion-ui/src/'
+  ],
+  javascript: [
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/what-input/what-input.js',
+    'bower_components/foundation-sites/js/foundation.core.js',
+    'bower_components/foundation-sites/js/foundation.util.*.js',
+    // Paths to individual JS components defined below
+    'bower_components/foundation-sites/js/foundation.abide.js',
+    'bower_components/foundation-sites/js/foundation.accordion.js',
+    'bower_components/foundation-sites/js/foundation.accordionMenu.js',
+    'bower_components/foundation-sites/js/foundation.drilldown.js',
+    'bower_components/foundation-sites/js/foundation.dropdown.js',
+    'bower_components/foundation-sites/js/foundation.dropdownMenu.js',
+    'bower_components/foundation-sites/js/foundation.equalizer.js',
+    'bower_components/foundation-sites/js/foundation.interchange.js',
+    'bower_components/foundation-sites/js/foundation.magellan.js',
+    'bower_components/foundation-sites/js/foundation.offcanvas.js',
+    'bower_components/foundation-sites/js/foundation.orbit.js',
+    'bower_components/foundation-sites/js/foundation.responsiveMenu.js',
+    'bower_components/foundation-sites/js/foundation.responsiveToggle.js',
+    'bower_components/foundation-sites/js/foundation.reveal.js',
+    'bower_components/foundation-sites/js/foundation.slider.js',
+    'bower_components/foundation-sites/js/foundation.sticky.js',
+    'bower_components/foundation-sites/js/foundation.tabs.js',
+    'bower_components/foundation-sites/js/foundation.toggler.js',
+    'bower_components/foundation-sites/js/foundation.tooltip.js',
+    'src/assets/js/**/*.js',
+    'src/assets/js/app.js'
+  ]
+};
+
+// Delete the "dist" folder
+// This happens every time a build starts
+gulp.task('clean', function(done) {
+  rimraf('dist', done);
+});
+
+// Copy files out of the assets folder
+// This task skips over the "img", "js", and "scss" folders, which are parsed separately
+gulp.task('copy', function() {
+  gulp.src(PATHS.assets)
+    .pipe(gulp.dest('dist/assets'));
+});
+
+// Copy page templates into finished HTML files
+gulp.task('pages', function() {
+  gulp.src('src/pages/**/*.{html,hbs,handlebars}')
+    .pipe(panini({
+      root: 'src/pages/',
+      layouts: 'src/layouts/',
+      partials: 'src/partials/',
+      data: 'src/data/',
+      helpers: 'src/helpers/'
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('pages:reset', function(cb) {
+  panini.refresh();
+  gulp.run('pages');
+  cb();
+});
+
+gulp.task('styleguide', function(cb) {
+  sherpa('src/styleguide/index.md', {
+    output: 'dist/styleguide.html',
+    template: 'src/styleguide/template.html'
+  }, cb);
+});
+
+// Compile Sass into CSS
+// In production, the CSS is compressed
+gulp.task('sass', function() {
+  var uncss = $.if(isProduction, $.uncss({
+    html: ['src/**/*.html'],
+    ignore: [
+      new RegExp('^meta\..*'),
+      new RegExp('^\.is-.*')
+    ]
+  }));
+
+  var minifycss = $.if(isProduction, $.minifyCss());
+
+  return gulp.src('src/assets/scss/app.scss')
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      includePaths: PATHS.sass
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: COMPATIBILITY
+    }))
+    .pipe(uncss)
+    .pipe(minifycss)
+    .pipe($.if(!isProduction, $.sourcemaps.write()))
+    .pipe(gulp.dest('dist/assets/css'));
+});
+
+// Combine JavaScript into one file
+// In production, the file is minified
+gulp.task('javascript', function() {
+  var uglify = $.if(isProduction, $.uglify()
+    .on('error', function (e) {
+      console.log(e);
+    }));
+
+  return gulp.src(PATHS.javascript)
+    .pipe($.sourcemaps.init())
+    .pipe($.concat('app.js'))
+    .pipe(uglify)
+    .pipe($.if(!isProduction, $.sourcemaps.write()))
+    .pipe(gulp.dest('dist/assets/js'));
+});
+
+// Copy images to the "dist" folder
+// In production, the images are compressed
+gulp.task('images', function() {
+  var imagemin = $.if(isProduction, $.imagemin({
+    progressive: true
+  }));
+
+  return gulp.src('src/assets/img/**/*')
+    .pipe(imagemin)
+    .pipe(gulp.dest('dist/assets/img'));
+});
+
+// Styleguide
+gulp.task('styleguide:generate', function() {
+  return gulp.src([
+      'src/assets/scss/base/*.scss',
+      'src/assets/scss/components/*.scss',
+      'src/assets/scss/helpers/*.scss',
+      'src/assets/scss/layout/*.scss',
+      'src/assets/scss/pages/*.scss',
+      'src/assets/scss/themes/*.scss',
+      'src/assets/scss/_overview.scss'
+    ])
+    .pipe(styleguide.generate({
+        basicAuth: {
+          username: 'admin',
+          password: 'admin'
+        },
+        title: 'Antelope - Styleguide',
+        server: true,
+        port: 3007,
+        rootPath: 'dist/docs/',
+        overviewPath: 'README.md'
+      }))
+    .pipe(gulp.dest('dist/docs/'));
+});
+
+gulp.task('styleguide:applystyles', function() {
+  return gulp.src([
+      'src/assets/scss/base/*.scss',
+      'src/assets/scss/components/*.scss',
+      'src/assets/scss/helpers/*.scss',
+      'src/assets/scss/layout/*.scss',
+      'src/assets/scss/pages/*.scss',
+      'src/assets/scss/themes/*.scss'
+    ])
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(styleguide.applyStyles())
+    .pipe(gulp.dest('dist/docs/'));
+});
+
+gulp.task('watch', ['styleguide'], function() {
+  // Start watching changes and update styleguide whenever changes are detected
+  // Styleguide automatically detects existing server instance
+  gulp.watch(['*.scss'], ['styleguide']);
+});
+
+gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
+
+// Regression tests
+gulp.task('update-tests', function() {
+  process.chdir(backstopJS);
+  var child = spawn('gulp', reference);
+  child.stdout.on('data', function(data) {
+      if (data) {
+          console.log(data.toString())
+      }
+  });
+});
+
+gulp.task('run-tests', function() {
+  process.chdir(backstopJS);
+  var child = spawn('gulp', test);
+  child.stdout.on('data', function(data) {
+      if (data) {
+          console.log(data.toString())
+      }
+  });
+});
+
+// Build the "dist" folder by running all of the above tasks
+gulp.task('build', function(done) {
+  sequence('clean', ['pages', 'sass', 'javascript', 'images', 'copy'], 'styleguide', done);
+});
+
+// Start a server with LiveReload to preview the site in
+gulp.task('server', ['build'], function() {
+  browser.init({
+    server: 'dist', port: PORT
+  });
+});
+
+// Build the site, run the server, and watch for file changes
+gulp.task('default', ['build', 'server'], function() {
+  gulp.watch(PATHS.assets, ['copy', browser.reload]);
+  gulp.watch(['src/pages/**/*.html'], ['pages', browser.reload]);
+  gulp.watch(['src/{layouts,partials}/**/*.html'], ['pages:reset', browser.reload]);
+  gulp.watch(['src/assets/scss/**/*.scss'], ['sass', browser.reload]);
+  gulp.watch(['src/assets/js/**/*.js'], ['javascript', browser.reload]);
+  gulp.watch(['src/assets/img/**/*'], ['images', browser.reload]);
+  gulp.watch(['src/styleguide/**'], ['styleguide', browser.reload]);
+});
---
+++ readme.md
@@ -0,0 +1,73 @@
+# Antelope #
+
+> Starter webapp framework created using the [Yeoman](http://yeoman.io) generator that scaffolds out a front-end web app using [Gulp](http://gulpjs.com/) for the build process, [Foundation 6](http://foundation.zurb.com/sites) for the SCSS framework, [BackstopJS](https://garris.github.io/BackstopJS) for the automatic regression tests and [SC5 Style guide generator](http://styleguide.sc5.io/) for creating a living Styleguide.
+
+
+### What is this repository for? ###
+
+Starting development on a new project or adding to an existing project? Install now to have a consistent framework with automation.
+
+
+### What does this repository do? ###
+
+* Built upon Foundation 6 from Zurb - http://foundation.zurb.com/sites/docs/
+* Automating CSS Regression Testing - phantomjs, casperjs and backstopjs
+* Automatic Styleguide creation - SC5 styleguide using KSS notation
+
+(Use this link for referencing classes and how to construct HTML pages using the correct classes)
+
+
+### Getting Started ###
+
+To get started download Antelope with Git:
+```
+git clone https://github.com/nilssanderson/antelope.git
+cd antelope
+npm install
+bower install
+```
+
+Then to build the framework:
+```
+gulp
+```
+
+The default task should:
+
+* Build out a static app
+* Convert SCSS to CSS
+* Generate a SC5 Styleguide based on the SCSS comments
+* Present a UI Elements page
+* Open these in the browser for viewing and updating (Styleguide)
+
+Your finished site will be created in a folder called `dist`, viewable at this URL:
+
+```
+http://localhost:8000
+```
+
+To create compressed, production-ready assets, run `gulp build`.
+
+
+### Setting up the Automatic CSS Regression Testing ###
+
+Follow the below instructions to set up the BackstopJS tests and update the backstop.json file as needed if one does not exist.
+Open the folder in your command line, and install the needed dependencies:
+```
+cd bower_components/BackstopJS
+npm install
+
+cd antelope/bower_components/BackstopJS
+gulp genConfig
+cd ../../
+```
+
+Run this after any change to the backstop.json file to update the references:
+```
+gulp update-tests
+```
+
+Then to run the tests:
+```
+gulp run-tests
+```
